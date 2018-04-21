8383a1eec59cf36a8cc63f65db9ddb160b53cf7c
孙国栋 <sunguodong@jd.com>
2018-04-21 18:00:26 +0800

    初始化

---
+++ temp/temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by 孙国栋 on 2018/4/21.
+//  Copyright © 2018年 孙国栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ temp/temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by 孙国栋 on 2018/4/21.
+//  Copyright © 2018年 孙国栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
